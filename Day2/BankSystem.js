094a9b9ab018dee63c65b781ed4296fe0c110e65
Armykookie35 <mercyojogo@gmail.com>
2025-09-08 12:46:38 +0100

    ...

--- Day2/BankSystem.js
+++ Day2/BankSystem.js
@@ -14,7 +14,18 @@ function addCustomer(name, balance,customId) {
   Customers.push(Customer);
   return Customer;
 }
-
+function addTransaction(type, amount, customId) {
+    const Transaction = {
+      type:type,
+    amount: amount,
+    customId: customId,
+  };
+  Transactions.push(Transaction);
+  return Transaction;
+}
+addTransaction("deposit", 5000, 1);
+addTransaction("withdraw", 2000, 2);
+addTransaction("deposit", 3000, 3);
 function add(a, b) {
   return a + b;
 }
@@ -51,3 +62,30 @@ function findCustomer(customId) {
   });
 }
 findCustomer(2);
+console.log("Transactions:", Transactions);
+// to find transactions by customId
+function findTransaction(customId) {
+    let transaction = {},
+        total = 0;
+    Transactions.map((id) => {
+        if (id.customId === customId) {
+            transaction = id;
+            total += id.amount;
+            return console.log("Transaction Details", transaction, "Total Amount:", total);
+        }
+    })
+}
+
+// to loo all transactions 
+    console.log("List of Transactions using a loop :");
+
+for (let i = 0; i < Transactions.length; i++){
+    let TR = Transactions[i];
+    console.log(TR);
+}
+// function to count the number of transactions 
+function countTransactions() {
+    return Transactions.length;
+}
+    console.log("Number of Transactions : ", countTransactions());
+
