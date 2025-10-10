5c954e7b70ac1a8d4a39731a9590fbc8577d9f4a
Armykookie35 <mercyojogo@gmail.com>
2025-10-10 11:11:27 +0100

    k

--- 30Days/CallBackFunctions.js
+++ 30Days/CallBackFunctions.js
@@ -66,5 +66,91 @@ function calculateMedian(ages) {
   return median;
 }
 console.log("The median is :" + calculateMedian(ages));
-const count = ages.length();
-console.log(count.toString());
+
+// day 10 
+const countries = new Set()
+console.log(countries)
+const setofAges = new Set(ages);
+for (const newSet of setofAges) {
+  console.log(newSet)
+}
+const numbers = [1,2,3,4,5,3,2,1]
+
+setofAges.add(70) 
+setofAges.add(7) 
+setofAges.add(40) 
+console.log(setofAges);
+console.log(setofAges.has(10));
+const total = [ ...ages]
+console.log(total)
+
+
+// Destructuring and spreading 
+const fullStack = [
+  ["HTML", "CSS", "JS", "React"],
+  ["Node", "Express", "MongoDB"],
+];
+const [frontEnd, backEnd] = fullStack;
+
+console.log(frontEnd);
+console.log(backEnd);
+
+const person = {
+  firstName: 'Asabeneh',
+  lastName: 'Yetayeh',
+  age: 250,
+  country: 'Finland',
+  job: 'Instructor and Developer',
+  skills: [
+    'HTML',
+    'CSS',
+    'JavaScript',
+    'React',
+    'Redux',
+    'Node',
+    'MongoDB',
+    'Python',
+    'D3.js'
+  ],
+  languages: ['Amharic', 'English', 'Suomi(Finnish)']
+}
+// function which give information about the person object without destructuring
+
+const getPersonInfo = obj => {
+  const skills = obj.skills
+  const formattedSkills = skills.slice(0, -1).join(', ')
+  const languages = obj.languages
+  const formattedLanguages = languages.slice(0, -1).join(', ')
+
+  personInfo = `${obj.firstName} ${obj.lastName} lives in ${obj.country}. He is  ${
+    obj.age
+  } years old. He is an ${obj.job}. He teaches ${formattedSkills} and ${
+    skills[skills.length - 1]
+  }. He speaks ${formattedLanguages} and a little bit of ${languages[2]}.`
+
+  return personInfo
+}
+
+console.log(getPersonInfo(person))
+const todoList = [
+  {
+    task: "Prepare JS Test",
+    time: "4/1/2020 8:30",
+    completed: true,
+  },
+  {
+    task: "Give JS Test",
+    time: "4/1/2020 10:00",
+    completed: false,
+  },
+  {
+    task: "Assess Test Result",
+    time: "4/1/2020 1:00",
+    completed: false,
+  },
+];
+
+for (const { task, time, completed } of todoList) {
+  console.log(task, time, completed);
+}
+
