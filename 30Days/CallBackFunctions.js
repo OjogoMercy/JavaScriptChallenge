140c499d3f72bc582d7c9afe98631f54369b395b
Armykookie35 <mercyojogo@gmail.com>
2025-10-16 12:29:36 +0100

    ...

--- 30Days/CallBackFunctions.js
+++ 30Days/CallBackFunctions.js
@@ -171,6 +171,48 @@ console.log(matchReplaced);
 // let pattern = 'Test'
 // let Regex = new RegexExp(pattern)
 
+const names = ["Asabeneh", "Brook", "David", "John"];
+console.table(names);
+const user = {
+  name: "Asabeneh",
+  title: "Programmer",
+  country: "Finland",
+  city: "Helsinki",
+  age: 250,
+};
+console.table(user);
+
+const users = [
+  {
+    name: "Asabeneh",
+    title: "Programmer",
+    country: "Finland",
+    city: "Helsinki",
+    age: 250,
+  },
+  {
+    name: "Eyob",
+    title: "Teacher",
+    country: "Sweden",
+    city: "London",
+    age: 25,
+  },
+  {
+    name: "Asab",
+    title: "Instructor",
+    country: "Norway",
+    city: "Oslo",
+    age: 22,
+  },
+  {
+    name: "Matias",
+    title: "Developer",
+    country: "Denmark",
+    city: "Copenhagen",
+    age: 28,
+  },
+];
+console.table(users);
 
 
 
