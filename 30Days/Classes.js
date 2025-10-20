716640457145647340615f01b77207c50c231b67
Armykookie35 <mercyojogo@gmail.com>
2025-10-20 10:31:07 +0100

    d

--- 30Days/Classes.js
+++ 30Days/Classes.js
@@ -4,9 +4,39 @@
 class Person {
     constructor(firstName, lastName) {
         console.log(this);
-        this.firstName = Mercy
-        this.lastName = Ojogo
+        this.firstName = firstName
+        this.lastName = lastName
     }
 }
-const person = new Person()
+const person = new Person('Mercy', 'Ojogo')
+const person1 = new Person('Martha', 'Ojogo')
+const person2 = new Person("Daniel", "Ojogo");
+
+class Info {
+  constructor(firstName, lastName, age, country, city) {
+    console.log(this) 
+    this.firstName = firstName
+    this.lastName = lastName
+    this.age = age
+    this.country = country
+    this.city = city
+  }
+}
+
+const person3 = new Info('Asabeneh', 'Yetayeh', 250, 'Finland', 'Helsinki')
+
+console.log(person3)
 console.log(person)
+console.log(person1)
+console.log(person2);
+
+// Inheritance from classes 
+class Student extends Person {
+  saySomething() {
+    console.log("I am a child of the person class");
+  }
+}
+
+const s1 = new Student("Asabeneh", "Yetayeh", "Finland", 250, "Helsinki");
+console.log(s1);
+console.log(s1.saySomething());
