291b664e560e04491d1dd9a5ce435cd35e0f0b28
Armykookie35 <mercyojogo@gmail.com>
2025-10-20 09:56:32 +0100

    ..

---
+++ 30Days/Classes.js
@@ -0,0 +1,12 @@
+
+// Day 15 Classes
+// Classes are used as blueprints for creating objects 
+class Person {
+    constructor(firstName, lastName) {
+        console.log(this);
+        this.firstName = Mercy
+        this.lastName = Ojogo
+    }
+}
+const person = new Person()
+console.log(person)
